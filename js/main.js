88cb8373a8e2729b2b52a7f2bfd410f6bf22319b
“Erika <foglzerika@gmail.com>
2017-04-19 11:22:58 -0500

    Creando elementos padres por js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+var nodoAbuelo=document.getElementById("principal");
+var nodoPadre= document.createElement("div");
+var nodoPadre2=document.createElement("div");
+nodoAbuelo.appendChild(nodoPadre);
