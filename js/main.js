0c0beb88cec48c81b0dfa1edb6217fa8729b4f91
“Erika <foglzerika@gmail.com>
2017-04-19 12:29:21 -0500

    Arreglando el bug de posicion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 
 var nodoAbuelo=document.getElementById("principal");
-var nodoPadre= document.createElement("div");
-var nodoPadre2=document.createElement("div");
+var nodoPadre= creaNodo();
+var nodoPadre2= creaNodo();
+//Les aderimos una clase "padres"
+nodoPadre.className="padres";
+nodoPadre2.className="padres";
+// Le aderimos el hijo
+nodoAbuelo.appendChild(nodoPadre2);
 nodoAbuelo.appendChild(nodoPadre);
+var coordenada_x=30;
+var coordenada_y=30;
+// Nos traemos el arreglo con todos los elementos que pertenescan a la clase
+var clasePadre =Array.from(document.getElementsByClassName("padres"));
+ console.log(clasePadre);
+clasePadre.forEach(function(elemento){
+  elemento.style.position="relative";
+   elemento.style.height="200px";
+   elemento.style.width="200px";
+   elemento.style.background= "#FF6F00";
+   elemento.style.top=coordenada_x+"px";
+   elemento.style.left=coordenada_y+"px";
+   coordenada_x+=30;
+});
+
+function creaNodo(){
+    return document.createElement("div");
+}
